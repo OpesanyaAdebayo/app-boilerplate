895cddb5c8dd08095004f40b6b9f9a3fffd3739d
Opesanya Adebayo <Bayo@onejsninja.local>
2017-10-07 00:14:26 +0100

    tested login and signup routes

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,7 +1,7 @@
 $("#loginform").submit(function(e){
     $("#err").empty();
     e.preventDefault();
-    $.post('/signup', $(this).serialize(), function(data) {
+    $.post('/login', $(this).serialize(), function(data) {
         console.log(data.errormsg);
         if(data.errormsg) {
             $("#err").append("<p>" + data.errormsg +"</p>")
