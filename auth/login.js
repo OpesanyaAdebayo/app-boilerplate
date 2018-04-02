d8fcb4ba839d221ab270a38ba16ab10b195a094f
Opesanya Adebayo <bayoopesanya@gmail.com>
2018-04-02 08:56:46 +0100

    improved login flow

--- auth/login.js
+++ auth/login.js
@@ -1,30 +1,25 @@
-var mongojs = require('mongojs');
-var db = mongojs(process.env.MLAB_URL); // Replace with credentials from .env file
-var bcrypt = require('bcryptjs');
-var salt = bcrypt.genSaltSync(10);
-let errormsg = "";
+let mongojs = require('mongojs');
+let bcrypt = require('bcryptjs');
+let db = mongojs(process.env.MLAB_URI, ['users']); // Replace with credentials from .env file
+let salt = bcrypt.genSaltSync(10);
 
-const userLogin = (formInput) => {
+const processFormInput = (formInput) => {
     return new Promise((resolve, reject) => {
         db.users.findOne({
             email: formInput.email
         }, (err, profile) => {
             if (err) {
                 return reject(Error(err));
-            } else if (profile === null) {
-                errormsg = "This email address has not been registered.";
-                resolve(errormsg);
-            } else {
-                if( bcrypt.compareSync(formInput.password, profile.password)) {
-                    resolve("User Succesfully logged in.");
-                }
-                resolve("Incorrect password.");
+            } else if (!profile || !bcrypt.compareSync(formInput.password, profile.password)) {
+                return reject(new Error('Invalid username or password.'));
             }
+            resolve(profile);
+
         });
     });
 };
 
 
 module.exports = {
-    userLogin: userLogin
+    processFormInput: processFormInput
 };
