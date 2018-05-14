3ac43da0b71fef0db86e94a9c8be21c4730ec2f2
Opesanya Adebayo <bayoopesanya@gmail.com>
2018-05-14 13:00:04 +0100

    adjusted tests

--- test/test.express.js
+++ test/test.express.js
@@ -1,45 +1,35 @@
 const supertest = require('supertest');
 const should = require('should');
+const app = require('../app');
 
-let server = supertest.agent("http://localhost:3000");
-
-describe("homepage should be available", function(){
-    it("should return a 200 response", function(done) {
-        server.get("/").expect(200).end(function(err,res) {
-            res.status.should.equal(200);
-            // res.body.error.should.equal(false);
-            done();
-        });
+describe("homepage should be available", function () {
+    it("should return a 200 response", function (done) {
+        supertest(app)
+            .get("/")
+            .expect(200, done);
     });
 });
 
 describe("login should be available", function(){
     it("should return a 200 response", function(done) {
-        server.get("/login").expect(200).end(function(err,res) {
-            res.status.should.equal(200);
-            // res.body.error.should.equal(false);
-            done();
-        });
+        supertest(app)
+        .get("/login")
+        .expect(200, done);
     });
 });
 
 describe("signup should be available", function(){
     it("should return a 200 response", function(done) {
-        server.get("/signup").expect(200).end(function(err,res) {
-            res.status.should.equal(200);
-            // res.body.error.should.equal(false);
-            done();
-        });
+        supertest(app)
+        .get("/signup")
+        .expect(200, done);
     });
 });
 
-describe("signup should be available", function(){
+describe("random route should be unavailable", function(){
     it("should return a 404 response", function(done) {
-        server.get("/random").expect(404).end(function(err,res) {
-            res.status.should.equal(404);
-            // res.body.error.should.equal(false);
-            done();
-        });
+        supertest(app)
+        .get("/random")
+        .expect(404, done)
     });
 });
-
